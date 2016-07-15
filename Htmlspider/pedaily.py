1280f0b687e7682f5be7f90ace0eb6d6816b2657
chenpeijie <chenpeijie@txcap.com>
2016-07-15 11:18:20 +0800

    新增投资界脚本，优化新芽脚本

--- Htmlspider/pedaily.py
+++ Htmlspider/pedaily.py
@@ -1,39 +1,38 @@
 # -*-coding: utf-8 -*-
-#新芽网
+#投资界
+import os
+
 from BeautifulSoup import BeautifulSoup
 from Clib import db
+from Clib.Threadhtml import Threadstart
 from Clib.http import _http
 from Clib.httplog import log
 from Clib.config_db import news
-from Clib.Threadhtml import Threadstart
+
+
 
 Classify={
-    'VC':['http://newseed.pedaily.cn/news/c4210-p',u'引力波'],
-    'FT':['http://newseed.pedaily.cn/news/c4093-p',u'访谈'],
-    'QS':['http://newseed.pedaily.cn/news/c4092-p',u'趋势'],
-    'CY':['http://newseed.pedaily.cn/news/t9-p',u'创业'],
-    'RZ':['http://newseed.pedaily.cn/news/t627-p',u'融资'],
-    'DJS':['http://newseed.pedaily.cn/news/t4717-p',u'独角兽'],
+    'PE':['http://pe.pedaily.cn/top/handlers/Handler.ashx?action=newslist-all&p=%s&url=http://pe.pedaily.cn/top/newslist.aspx?p=1',u'VC/PE'],
+    'TMT':['http://www.pedaily.cn/top/handlers/Handler.ashx?action=newslist-all&p=%s&url=http://www.pedaily.cn/top/newslist.aspx?c=i-tmt/',u'TMT'],
+    'XF':['http://www.pedaily.cn/top/handlers/Handler.ashx?action=newslist-all&p=%s&url=http://www.pedaily.cn/top/newslist.aspx?c=i-consume/',u'消费'],
+    'YL':['http://www.pedaily.cn/top/handlers/Handler.ashx?action=newslist-all&p=%s&url=http://www.pedaily.cn/top/newslist.aspx?c=i-medical-health/',u'医疗健康']
 }
 
 
-
-class mian():
+class work():
     def __init__(self):
-        self.type='0'#全量更新
-        self.sorts=['VC','FT','QS','CY','RZ','DJS']#分类
-        self.url_tell='newseed.pedaily.cn'#验证域名
+        self.website=u'投资界'
         self.time='2016-01-01 00:00'
-        self.table=news['table']
-        self.website=u'新芽'
 
-    def newseed_text(self,list,olddata=''):
+
+    def pedaily_text(self,list):
         '''
         :param list[req_url,req_url,category]
         '''
-
-        req_url = list[0]
-        img_url = list[1]
+        req_url=list[0]
+        img_url=list[1]
+        if req_url.split('/')[2]=='newseed.pedaily.cn':
+            return
         try:
             arr = {}
             # 获取正文信息
@@ -41,18 +40,17 @@ class mian():
             htmlSource = http.get_data(req_url=req_url, num=2)
             soup = BeautifulSoup(htmlSource)
             try:
-                arr['website'] = u"新芽"
-                arr['title'] = soup.find('div', {"class": "col-md-660"}).h1.getText()
+                arr['website'] = u"投资界"
+                arr['title'] = soup.find('div', {"class": "news-show"}).h1.getText()
                 arr['introduction'] = soup.find('div', {"class": "subject"}).getText()
                 text = []
-                p=soup.find('div', {"class": "pull-left"})
-                for i in p.findAll('a'):
+                for i in soup.find('div', {"class": "box-l tag"}).contents:
                     text.append(i.getText())
                 arr['keyword'] = ','.join(text)
 
                 # 时间，来源，作者
-                content = soup.find('div', {"class": "info"}).findAll('div')[2].contents
-                content = content[2].split(u" ")
+                content = soup.find('div', {"class": "box-l"}).contents
+                content = content[3].split(u" ")
                 arr['author'] = ''
                 arr['source'] = ''
                 try:arr['author'] = content[2]
@@ -70,32 +68,36 @@ class mian():
             except Exception,E:
                 log('http').log.warning(req_url)
                 log('http').log.warning(E)
-                #去重字段
             key={'title':arr['title'],'category':arr['category']}
-            db.insert_dict(table=self.table, repeat=4,key=key,**arr)
+            db.insert_dict(table=news['table'], repeat=4,key=key, **arr)
         except Exception,E:
             log('http').log.warning(E)
 
 
-    def sorts_list(self,page,sorts,olddata=''):
+    def olddata(self,category):
+        sql="SELECT `title`, `release_time` FROM `%s` WHERE `category` = '%s' and `website`='%s'ORDER BY `release_time` DESC limit 1"%(news['table'],category,self.website)
+        return db.select(sql)
+
+
+
+    def list(self,page,sorts,olddata=''):
         '''
         :param page: 页数
         :param sorts: 分类
         :param olddata: 数据库数据，用于增量
         :return:
         '''
-        url=Classify[sorts][0]+str(page)
+        url=Classify[sorts][0]%page
+        print url
         self.category=Classify[sorts][1]
+        print self.category
         urlhttp = _http()
         htmlSource = urlhttp.get_data(req_url=url, num=2)
         soup = BeautifulSoup(htmlSource)
         list = []
         title=''
-
         try:
-            ul=soup.find('ul',{"id": "newslist"})
-            for i in ul.findAll('li'):
-
+            for i in soup.findAll('li'):
                 data = []
                 data.append(i.find('div', {"class": "img"}).find('a').get("href"))
                 data.append(i.find('div', {"class": "img"}).find('img').get("src"))
@@ -103,41 +105,24 @@ class mian():
                 title = i.find('div', {"class": "img"}).find('a').find('img').get("alt")
                 time = i.find('span', {"class": "date"}).getText()
                 if time<self.time:
-                    Threadstart(self.newseed_text,list,num=3)
+                    Threadstart(self.pedaily_text,list,3)
                     return False
                 #增量添加，查询匹配时返回
                 if olddata:
-                    print title,olddata[0]['title'],olddata[0]['release_time'],time
+                    print title,olddata[0]['title']
                     if time < str(olddata[0]['release_time']) or olddata[0]['title'] == title:
-                        list.pop(-1)
-                        Threadstart(self.newseed_text,list,num=3)
+                        Threadstart(self.pedaily_text,list,3)
                         return False
-            Threadstart(self.newseed_text,list,num=3)
         except Exception,E:
             log('http').log.warning(url)
             log('http').log.warning(E)
-
-    def olddata(self,category):
-        '''
-        :param website: 网站名称
-        :param category: 分类名称
-        :return: title，release_time
-        '''
-
-        sql="SELECT `title`, `release_time` FROM `%s` WHERE `category` = '%s' and `website`='%s'ORDER BY `release_time` DESC limit 1"%(self.table,category,self.website)
-        return db.select(sql)
-
+        return True
 
 
 if __name__ == "__main__":
-    xinya=mian()
-    for sort in xinya.sorts:
-        sorts=Classify[sort][1]
-        sqldata=xinya.olddata(sorts)
-        for page in range(1,100):
-            if xinya.sorts_list(page,sort,sqldata)==False:
+    work=work()
+    for sorts in Classify:
+        olddata=work.olddata(Classify[sorts][1])
+        for page in range(1,200):
+            if work.list(page,sorts,olddata)==False:
                 break
-
-
-
-
